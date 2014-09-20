fa0680b951801a35e189bd7d0870d7b62ba78ea4
Pau Fracés <pfraces@gmail.com>
2014-09-20 22:53:11 +0200

    Update domo.js

--- domo.js
+++ domo.js
@@ -1,17 +1,17 @@
 define('domo', function (require) {
   'use strict';
   
-  var is  = require('mu.is'),
-      fn  = require('mu.fn'),
-      api = require('mu.api');
+  var isString = require('mu.is.string'),
+      partial  = require('mu.fn.partial'),
+      plug     = require('mu.api.plug');
 
   var query = function (selector, context) {
     context = context || document;
-    if (is.string(selector)) { return context.querySelectorAll(selector); }
+    if (isString(selector)) { return context.querySelectorAll(selector); }
     return selector;
   };
   
   return {
-    use: fn.partial(api.plug, query)
+    use: partial(plug, query)
   };
 });
