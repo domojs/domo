3f20be091fb8b61fa577f81612656d98d020bcf0
Pau Fracés <pfraces@gmail.com>
2015-04-12 01:20:22 +0200

    moved index and parent to extra-plugins

--- domo.js
+++ domo.js
@@ -183,30 +183,6 @@ define('domo.val', function (require) {
   return val;
 });
 
-define('domo.index', function (require) {
-  'use strict';
-
-  var each = require('mu.list.each');
-
-  var index = function (node) {
-    return each(node.parentNode.children, function (item, index) {
-      if (item === node) { return index; }
-    });
-  };
-
-  return index;
-});
-
-define('domo.parent', function () {
-  'use strict';
-
-  var parent = function (node) {
-    return node.parentNode;
-  };
-
-  return parent;
-});
-
 define('domo.clone', function () {
   'use strict';
 
